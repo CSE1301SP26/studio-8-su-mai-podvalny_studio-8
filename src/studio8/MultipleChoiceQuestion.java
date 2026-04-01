3237675990953c6d75c3efe10689b56db751aaa4
elizapodvalny <e.podvalny@wustl.edu>
2026-04-01 15:10:08 -0500

    half done

--- src/studio8/MultipleChoiceQuestion.java
+++ src/studio8/MultipleChoiceQuestion.java
@@ -3,7 +3,7 @@ package studio8;
 import support.cse131.NotYetImplementedException;
 
 public class MultipleChoiceQuestion extends Question {
-
+ private String [] choices;
 	/**
 	 * Constructor
 	 * @param prompt
@@ -15,7 +15,8 @@ public class MultipleChoiceQuestion extends Question {
 		// Call the super class constructor, then create and set
 		// instance variables for any values that aren't handled
 		// by the base class
-		throw new NotYetImplementedException();
+		super(prompt, answer, points);
+		this.choices = choices;
 	}
 	
 	/**
@@ -23,7 +24,11 @@ public class MultipleChoiceQuestion extends Question {
 	 * the choices present for the question.
 	 */
 	public void displayPrompt() {
-		throw new NotYetImplementedException();
+		super.displayPrompt();
+	String [] choices = this.getChoices();
+	for (int i = 0; i < choices.length; i++) {
+		System.out.println(i + 1 + ". " + choices[i]);
+	}
 	}
 	
 	/**
@@ -31,11 +36,13 @@ public class MultipleChoiceQuestion extends Question {
 	 * @return String[] of choices
 	 */
 	public String[] getChoices() {
-		throw new NotYetImplementedException();
+		return choices;
 	}
 	
 	public static void main(String[] args) {
-		// TODO: create your own MultipleChoiceQuestion
+		String [] answers = {"131", "231", "425", "1301"};
+		MultipleChoiceQuestion mq = new MultipleChoiceQuestion("If the options are 131, 231, 425, and 1301, which is larger?", "1301", 10, answers );
+		mq.displayPrompt();
 	}
 
 }
